bbc318be19bea0b76cf63f9daead756e6ec7d233
Lucas Simas <simas.lucas@hotmail.com>
2021-04-10 22:44:38 +0100

    Adjusting Example

--- Example/Program.cs
+++ Example/Program.cs
@@ -1,5 +1,10 @@
 ﻿using Desktop.Robot;
 using Desktop.Robot.Extensions;
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
 namespace Example
 {
     public static class Program
@@ -7,11 +12,15 @@ namespace Example
         static void Main(string[] args)
         {
             var robot = new Robot();
+            robot.OnMouseMove().Subscribe(position => Console.WriteLine(position));
             robot.AutoDelay = 1000;
             robot.MouseMove(1000, 1000);
             robot.Click();
             robot.Type("A cat is using my PC", 25);
             robot.CombineKeys(Key.Alt, Key.Tab);
+            using var screenshot = robot.CreateScreenCapture(new Rectangle(100, 100, 200, 200));
+            var path = Path.Combine(Directory.GetCurrentDirectory(), $"image-{Guid.NewGuid()}.bmp");
+            screenshot.Save(path, ImageFormat.Bmp);
         }
     }
 }
