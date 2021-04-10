d8a725cc3b58fbc5107817dbcf7eb5055be0a892
Lucas Simas <simas.lucas@hotmail.com>
2021-04-10 22:43:20 +0100

    Update System.Drawing and Implementing OnMouseMove Observable

---
+++ Desktop.Robot/Extensions/ListenersExtension.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Reactive.Concurrency;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace Desktop.Robot.Extensions
+{
+    public static class ListenersExtension
+    {
+        private static Point position = Point.Empty;
+
+        public static IObservable<Point> OnMouseMove(this IRobot robot)
+        {
+            position = robot.GetMousePosition();
+            var scheduler = new EventLoopScheduler(ts => new Thread(ts));
+            return Observable.Create<Point>(observer =>
+            {
+                var newPosition = robot.GetMousePosition();
+                if (position != newPosition)
+                {
+                    observer.OnNext(newPosition);
+                }
+                position = newPosition;
+                observer.OnCompleted();
+                return Disposable.Empty;
+            })
+            .ObserveOn(scheduler)
+            .Repeat();
+        }
+    }
+}
