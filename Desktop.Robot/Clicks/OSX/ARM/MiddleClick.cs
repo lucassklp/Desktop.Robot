f0f943331fc1d270802cd0a2a9d1556119c1257d
Chris Hanna <chris@beamable.com>
2024-06-24 07:06:34 -0400

    adds support for click extensions on osx arm

---
+++ Desktop.Robot/Clicks/OSX/ARM/MiddleClick.cs
@@ -0,0 +1,17 @@
+﻿namespace Desktop.Robot.Clicks.OSX.ARM
+{
+    internal record MiddleClick(int delay) : IClick
+    {
+        public int Delay => delay;
+
+        public void ExecuteMouseDown(MouseContext context)
+        {
+            Common.OtherClickDown((uint)context.Position.X, (uint)context.Position.Y);
+        }
+
+        public void ExecuteMouseUp(MouseContext context)
+        {
+            Common.OtherClickUp((uint)context.Position.X, (uint)context.Position.Y);
+        }
+    }
+}
