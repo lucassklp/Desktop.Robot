7bc2c09cbabbd69145def656bff0c8380045c013
Chris Hanna <cdhannaphone@gmail.com>
2024-08-19 14:14:00 -0400

    adds support for click extensions on osx arm (#58)
    Co-authored-by: Chris Hanna <chris@beamable.com>

---
+++ Desktop.Robot/Clicks/OSX/ARM/LeftClick.cs
@@ -0,0 +1,17 @@
+﻿namespace Desktop.Robot.Clicks.OSX.ARM
+{
+    internal record LeftClick(int delay) : IClick
+    {
+        public int Delay => delay;
+
+        public void ExecuteMouseDown(MouseContext context)
+        {
+            Common.LeftClickDown((uint)context.Position.X, (uint)context.Position.Y);
+        }
+
+        public void ExecuteMouseUp(MouseContext context)
+        {
+            Common.LeftClickUp((uint)context.Position.X, (uint)context.Position.Y);
+        }
+    }
+}
